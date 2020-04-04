696f4ea78376054a13b848c24a8ca533c7302ed0
Fergus Waugh <fjswaugh@gmail.com>
2020-04-04 15:47:06 +0100

    Added a Game class for keeping track of move history and so forth

---
+++ include/rock/game.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "types.h"
+#include <optional>
+#include <vector>
+
+namespace rock
+{
+
+struct Game
+{
+    Game();
+
+    static auto from_position(Position const&) -> Game;
+    static auto standard_new_game() -> Game;
+
+    auto num_moves_played() const -> std::size_t;
+    auto size() const -> std::size_t;
+    auto is_empty() const -> bool;
+
+    auto current_position() const -> Position const&;
+    auto current_status() const -> GameOutcome;
+    auto most_recent_move() const -> Move;
+
+    /**
+     * Returns true if move is legal
+     */
+    auto make_move(Move) -> bool;
+
+    auto undo_move() -> std::optional<Move>;
+    auto redo_move() -> std::optional<Move>;
+    auto reset_to_start() -> void;
+
+private:
+    Game(std::vector<Position>, std::vector<Move>);
+
+    std::vector<Position> history_{};
+    std::vector<Move> moves_{};
+    std::size_t i_{};
+};
+
+}  // namespace rock
---
+++ src/game.cpp
@@ -0,0 +1,97 @@
+#include "rock/game.h"
+#include "rock/algorithms.h"
+#include "rock/starting_position.h"
+#include <cassert>
+
+namespace rock
+{
+
+Game::Game() = default;
+
+Game::Game(std::vector<Position> history, std::vector<Move> moves)
+    : history_{std::move(history)}, moves_{std::move(moves)}
+{}
+
+auto Game::from_position(Position const& position) -> Game
+{
+    return Game{{position}, {}};
+}
+
+auto Game::standard_new_game() -> Game
+{
+    return Game{{rock::starting_position}, {}};
+}
+
+auto Game::num_moves_played() const -> std::size_t
+{
+    return moves_.size();
+}
+
+auto Game::size() const -> std::size_t
+{
+    return history_.size();
+}
+
+auto Game::is_empty() const -> bool
+{
+    return history_.empty();
+}
+
+auto Game::current_position() const -> Position const&
+{
+    return history_[i_];
+}
+
+auto Game::current_status() const -> GameOutcome
+{
+    return rock::get_game_outcome(current_position());
+}
+
+auto Game::most_recent_move() const -> Move
+{
+    assert(i > 0);
+    return moves_[i_ - 1];
+}
+
+auto Game::make_move(Move move) -> bool
+{
+    assert(!history_.empty());
+
+    if (!rock::is_legal_move(move, current_position()))
+        return false;
+
+    history_.erase(history_.begin() + static_cast<std::ptrdiff_t>(i_) + 1, history_.end());
+    moves_.erase(moves_.begin() + static_cast<std::ptrdiff_t>(i_), moves_.end());
+
+    moves_.push_back(move);
+    history_.push_back(rock::apply_move(move, current_position()));
+    ++i_;
+
+    return true;
+}
+
+auto Game::undo_move() -> std::optional<Move>
+{
+    if (i_ == 0)
+        return std::nullopt;
+
+    auto const move = most_recent_move();
+    --i_;
+    return move;
+}
+
+auto Game::redo_move() -> std::optional<Move>
+{
+    if (i_ + 1 >= size())
+        return std::nullopt;
+
+    ++i_;
+    return most_recent_move();
+}
+
+auto Game::reset_to_start() -> void
+{
+    i_ = 0;
+}
+
+}  // namespace rock
