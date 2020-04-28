d482f5683f340ee0947367aec7c2736d2555df99
Fergus Waugh <fjswaugh@gmail.com>
2020-04-28 18:50:11 +0100

    Fix up lazily-made header

--- include/rock/starting_position.h
+++ include/rock/starting_position.h
@@ -1,17 +1,18 @@
+#pragma once
 #include "parse.h"
 
 namespace rock
 {
 
-constexpr auto starting_board = parse_literal_board(" wwwwww "
-                                                    "b      b"
-                                                    "b      b"
-                                                    "b      b"
-                                                    "b      b"
-                                                    "b      b"
-                                                    "b      b"
-                                                    " wwwwww ");
+inline constexpr auto starting_board = parse_literal_board(" wwwwww "
+                                                           "b      b"
+                                                           "b      b"
+                                                           "b      b"
+                                                           "b      b"
+                                                           "b      b"
+                                                           "b      b"
+                                                           " wwwwww ");
 
-constexpr auto starting_position = Position{starting_board, Player::White};
+inline constexpr auto starting_position = Position{starting_board, Player::White};
 
 }  // namespace rock
