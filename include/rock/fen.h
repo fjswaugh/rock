4e78ce9bde6d41bb20bebf37224d4474d9d9a998
Fergus Waugh <fjswaugh@gmail.com>
2020-05-17 18:44:33 +0100

    Add fen functionality for Position (Board + player_to_move)

--- include/rock/fen.h
+++ include/rock/fen.h
@@ -8,7 +8,10 @@
 namespace rock
 {
 
-ROCK_API auto format_as_fen(rock::Board const&) -> std::string;
-ROCK_API auto parse_fen_to_board(std::string_view fen_string) -> std::optional<rock::Board>;
+ROCK_API auto format_as_fen(Board const&) -> std::string;
+ROCK_API auto format_as_fen(Position const&) -> std::string;
+
+ROCK_API auto parse_fen_to_board(std::string_view fen_string) -> std::optional<Board>;
+ROCK_API auto parse_fen_to_position(std::string_view fen_string) -> std::optional<Position>;
 
 }  // namespace rock
--- src/fen.cpp
+++ src/fen.cpp
@@ -1,9 +1,10 @@
 #include "rock/fen.h"
+#include <algorithm>
 
 namespace rock
 {
 
-auto format_as_fen(rock::Board const& b) -> std::string
+auto format_as_fen(Board const& b) -> std::string
 {
     auto res = std::string{};
 
@@ -19,8 +20,8 @@ auto format_as_fen(rock::Board const& b) -> std::string
 
         for (auto x = 0; x < 8; ++x)
         {
-            bool const is_white = b[rock::Player::White].at({x, y});
-            bool const is_black = b[rock::Player::Black].at({x, y});
+            bool const is_white = b[Player::White].at({x, y});
+            bool const is_black = b[Player::Black].at({x, y});
 
             if (is_white || is_black)
                 flush(n);
@@ -41,9 +42,17 @@ auto format_as_fen(rock::Board const& b) -> std::string
     return res;
 }
 
-auto parse_fen_to_board(std::string_view fen) -> std::optional<rock::Board>
+auto format_as_fen(Position const& p) -> std::string
 {
-    auto res = rock::Board{};
+    auto str = format_as_fen(p.board());
+    str.push_back(' ');
+    str.push_back(p.player_to_move() == Player::White ? 'w' : 'b');
+    return str;
+}
+
+auto parse_fen_to_board(std::string_view fen) -> std::optional<Board>
+{
+    auto res = Board{};
     auto x = int{};
     auto y = int{7};
 
@@ -60,9 +69,9 @@ auto parse_fen_to_board(std::string_view fen) -> std::optional<rock::Board>
             return std::nullopt;
 
         if (ch == 'P')
-            res[rock::Player::White].set_bit({x++, y});
+            res[Player::White].set_bit({x++, y});
         else if (ch == 'p')
-            res[rock::Player::Black].set_bit({x++, y});
+            res[Player::Black].set_bit({x++, y});
         else if (ch >= '0' && ch <= '9')
             x += ch - '0';
         else
@@ -70,6 +79,24 @@ auto parse_fen_to_board(std::string_view fen) -> std::optional<rock::Board>
     }
 
     return res;
+}
+
+auto parse_fen_to_position(std::string_view fen_string) -> std::optional<Position>
+{
+    auto board = parse_fen_to_board(fen_string);
+    if (!board)
+        return std::nullopt;
+
+    auto it = std::find(fen_string.begin(), fen_string.end(), ' ');
+    if (it == fen_string.end() || ++it == fen_string.end())
+        return std::nullopt;
+
+    if (*it == 'w')
+        return Position{*board, Player::White};
+    else if (*it == 'b')
+        return Position{*board, Player::Black};
+    else
+        return std::nullopt;
 }
 
 }  // namespace rock
